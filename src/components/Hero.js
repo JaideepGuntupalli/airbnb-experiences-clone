0f8593e6330ccdd49003f5062e2ccfae12eaa96c
Jaideep <guntupallijaideep@gmail.com>
2022-02-28 20:50:43 +0530

    improved responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,20 +11,20 @@ import pic9 from "../assets/hero/9.png";
 export default function Hero() {
     return (
         <div className="flex items-center gap-2 w-4/5 my-4 mx-auto md:my-8 md:w-1/2">
-            <img src={pic1} alt="" className="w-1/4 md:w-1/5" />
-            <div className="flex flex-col items-center gap-2 py-10 md:pb-24 w-1/4 md:w-1/5">
+            <img src={pic1} alt="" className="hidden md:block w-1/3 md:w-1/5" />
+            <div className="flex flex-col items-center gap-2 py-10 pb-16 md:pb-24 w-1/3 md:w-1/5">
                 <img src={pic2} alt="" className="w-full" />
                 <img src={pic3} alt="" className="w-full" />
             </div>
-            <div className="flex flex-col items-center gap-2 self-end w-1/4 md:w-1/5">
+            <div className="flex flex-col items-center gap-2 self-end w-1/3 md:w-1/5">
                 <img src={pic4} alt="" className="w-full" />
                 <img src={pic5} alt="" className="w-full" />
             </div>
-            <div className="flex flex-col items-center gap-2 self-start w-1/4 md:w-1/5">
+            <div className="flex flex-col items-center gap-2 self-start w-1/3 md:w-1/5">
                 <img src={pic6} alt="" className="w-full" />
                 <img src={pic7} alt="" className="w-full" />
             </div>
-            <div className=" hidden md:flex flex-col items-center gap-2 w-1/4 md:w-1/5">
+            <div className=" hidden md:flex flex-col items-center gap-2 w-1/5">
                 <img src={pic8} alt="" className="w-full" />
                 <img src={pic9} alt="" className="w-full" />
             </div>
