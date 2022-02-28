25ea3c0af22417f7c4181523f4cbedbfe1348fb9
Jaideep <guntupallijaideep@gmail.com>
2022-02-28 20:47:40 +0530

    Added to the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from "./logo.svg";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import OnlineExp from "./components/OnlineExp";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <Header />
+            <main className="flex flex-col items-center gap-10">
+                <Hero />
+                <OnlineExp />
+            </main>
+        </>
     );
 }
 
---
+++ src/components/OnlineExp.js
@@ -0,0 +1,7 @@
+export default function OnlineExp() {
+    return (
+        <div className="">
+            <h2>Oh god</h2>
+        </div>
+    );
+}
